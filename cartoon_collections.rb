c7756f9def2241b81a617f0b8be8a461d6ccc03e
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 21:27:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(dwarf_names)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array_calls)
+  array_calls.collect do |call|
+    call.capitalize
 end
 
 def long_planeteer_calls# code an argument here
@@ -16,3 +17,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
