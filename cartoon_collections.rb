4865c8d90ae833af19919aa46ba531196a4af6f5
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 21:34:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ def long_planeteer_calls(array_calls)
   array_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  #cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array_of_strings)
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
